c5512320f64fc3ae0948917962b3b953c80f007c
Eksoga <eksoga@mail.ru>
2016-08-24 12:11:22 +0300

    add files

---
+++ index.js
@@ -0,0 +1,173 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+var path = require('path');
+var sensor = require('ds18x20');
+var moment = require('moment');
+
+var spawn = require('child_process').spawn;
+var config = require('./config');
+
+var proc;
+var sensorObjects = [];
+
+var sensorLib = require("node-dht-sensor");
+
+function findSensors(){
+    var sensors = [];
+	var pins = config.get('sensorsPins');
+	for (var pinId in pins) {
+        if (pins[pinId].type == "DHT") {
+            sensorObjects.push({
+                address: pinId,
+                model: pins[pinId].model,
+                temperature: null,
+                humidity: null,
+                read: function() {
+                    var data = sensorLib.readSpec(this.model, this.address);
+                    this.temperature = data.temperature.toFixed(1)
+                    this.humidity = data.humidity.toFixed(1)
+                    return this;
+                }
+            });
+        } else if (pins[pinId].type == "DS18X20") {
+            let sensors = sensor.list();
+            if (sensors.length) {
+                for (let i = 0; i < sensors.length; i++){
+                    sensorObjects.push({
+                        address: sensors[i],
+                        temperature: null,
+                        read: function() {
+                            this.temperature = sensor.get(this.address);
+                            return this;
+                        }
+
+                    });
+                }
+            }
+        }
+    }
+    return sensorObjects;
+}
+
+function readSensors() {
+    if (sensorObjects.length == 0){
+        findSensors();
+    }
+    
+    var dataSensors = [];
+    for (let i = 0; i < sensorObjects.length; i++){
+        let sensor = sensorObjects[i].read();
+        dataSensors.push({
+            address: sensor.address,
+            temperature: sensor.temperature,
+            humidity: sensor.humidity,
+            name: null,
+        });
+    }
+
+    return dataSensors;
+}
+
+
+
+app.use('/', express.static(path.join(__dirname, 'stream')));
+
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+var sockets = {};
+moment.locale('ru');
+var currentTimeStr = moment().format('LLLL');
+
+io.on('connection', function(socket) {
+
+  sockets[socket.id] = socket;
+  console.log("Total clients connected : ", Object.keys(sockets).length);
+
+  socket.emit('currentTime', currentTimeStr);
+  socket.emit('clientInit', config.get('channels'));
+
+  socket.on('disconnect', function() {
+    delete sockets[socket.id];
+
+    // no more sockets, kill the stream
+    if (Object.keys(sockets).length == 0) {
+      app.set('watchingFile', false);
+      if (proc) proc.kill();
+      fs.unwatchFile('./stream/image_stream.jpg');
+    }
+  });
+
+  socket.on('dimming', function(data) {
+    socket.broadcast.emit('dimming', data);
+    config.set('channels:' + data.channel + ':value', data.value);
+    config.save();
+  });
+
+  socket.on('start-stream', function() {
+    if (app.get('watchingFile')) {
+      io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+      return;
+    }
+
+    var args = [
+        //"-n", "",
+      "-w", "640",
+      "-h", "480",
+      "-o", "-",
+      //"-t", "999999999",
+      //"-tl", "100",
+
+    ];
+    proc = spawn('raspistill', args);
+
+      var data = '';
+
+      stream.on('readable', function() {
+          data += stream.read().toString('base64');
+      });
+      stream.on('end', function() {
+          console.log(data);
+      });
+
+    console.log('Watching for changes...');
+
+    app.set('watchingFile', true);
+
+    fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
+      io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+    });
+  });
+
+});
+
+http.listen(config.get('port'), function() {
+  console.log('listening on *:' + config.get('port'));
+});
+
+var timerId = setInterval(function() {
+  var t = readSensors();
+  io.sockets.emit('tempChange', t);
+}, 3000);
+
+var timeTimerId = setInterval(function() {
+  var t = moment().format('LLLL');
+  if (t.localeCompare(currentTimeStr) !== 0) {
+      currentTimeStr = t;
+      io.sockets.emit('currentTime', currentTimeStr);
+  }
+}, 1000);
+
+function stopStreaming() {
+  if (Object.keys(sockets).length == 0) {
+    app.set('watchingFile', false);
+    if (proc) proc.kill();
+    fs.unwatchFile('./stream/image_stream.jpg');
+  }
+}
+
